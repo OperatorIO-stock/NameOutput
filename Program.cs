dacefe9449ea395183c6f6f63eb7a57346e39a1e
operatorIO-stock <vova.chigorin@yandex.com>
2024-12-02 19:50:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NameOutput;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your token:");
+        char tokenUser = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.WriteLine("Write your name: ");
+        string userName = Console.ReadLine();
+
+        int offsetFromEdge = 2;
+
+        int frameHeight = 3;
+        int frameLength = userName.Length + offsetFromEdge;
+
+        for (int i = 0; i < frameHeight; i++)
+        {
+            if (i == frameHeight / 2)
+            {
+                Console.WriteLine($"{tokenUser}{userName}{tokenUser}");
+            }
+            else 
+            {
+                Console.WriteLine(new string(tokenUser, frameLength));
+            }
+        }
+    }
+}
